d03670717b8f5111f91b7c1f3cb0c13683ae7fd8
boyeClaude <boye.claude@gmail.com>
2022-09-06 11:41:26 +0000

    Updated:Home page component
    - feat:create form component to get number phone from the user
    - fix: updated style on footer, footer-card and home components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,11 +6,21 @@ import { HomeComponent } from './home.component';
 import { HowItWorksComponent } from './components/how-it-works/how-it-works.component';
 import { HomeCardComponent } from './components/home-card/home-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
+import { FooterCardComponent } from '../components/footer-card/footer-card.component';
+import { FormComponent } from '../components/form/form.component';
 
 export const homeRoutes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  declarations: [StepCardComponent, HomeComponent, HowItWorksComponent, HomeCardComponent, SmallCardComponent],
+  declarations: [
+    StepCardComponent,
+    HomeComponent,
+    HowItWorksComponent,
+    HomeCardComponent,
+    SmallCardComponent,
+    FooterCardComponent,
+    FormComponent
+  ],
   imports: [CommonModule, RouterModule.forChild(homeRoutes)],
 })
 export class HomeModule {}
